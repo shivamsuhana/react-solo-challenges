1c1157cdd1b4dd409b0b538213b1f85d802d05ec
Shiv <shivamsuhana649@gmail.com>
2026-07-16 01:46:37 +0530

    challenge 2 commiting again

---
+++ courses/02-redux-rtk-query/project/src/store/slices/counterSlice.ts
@@ -0,0 +1,17 @@
+// createSlice import karo Redux Toolkit se
+import { createSlice } from '@reduxjs/toolkit'
+
+const counterSlice = createSlice({
+  name: 'counter',   
+  initialState: 0,   
+  reducers: {
+    
+    increment: (state) => state + 1,
+
+    decrement: (state) => state - 1,
+  }
+})
+
+export const { increment, decrement } = counterSlice.actions
+
+export default counterSlice.reducer
